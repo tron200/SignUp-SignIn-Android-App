22585b57fd550bf87c6dfa03ab62c73b72063fd5
Abdelrhman Mohamed Ezz-Eldin <62518324+tron200@users.noreply.github.com>
2021-10-28 05:03:55 +0200

    Layout Done !!

--- app/src/main/java/com/example/signup_signin/MainActivity.kt
+++ app/src/main/java/com/example/signup_signin/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.signup_signin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
+        var signIn :Boolean = false
+        val hyper: TextView = findViewById(R.id.hyper_link)
+        val header: TextView = findViewById(R.id.head)
+        val username: EditText = findViewById(R.id.username_edit_text)
+        val email: EditText = findViewById(R.id.email_edit_text)
+        val password: EditText = findViewById(R.id.password_edit_text)
+        val gender:RadioGroup = findViewById(R.id.radioGroup)
+        val signButton : Button = findViewById(R.id.sign_button)
+        hyper.setOnClickListener {
+            if (signIn){
+                signIn = false
+                hyper.setText(R.string.sign_up_insted)
+                header.setText(R.string.sign_in)
+                signButton.setText(R.string.sign_in)
+                email.visibility = View.GONE
+                gender.visibility = View.GONE
+            }else{
+                signIn = true
+                header.setText(R.string.sign_up)
+                hyper.setText(R.string.sign_in_insted)
+                signButton.setText(R.string.sign_up)
+                email.visibility = View.VISIBLE
+                gender.visibility = View.VISIBLE
+            }
+        }
     }
+
+    fun onRadioButtonClicked(view: android.view.View) {}
 }
